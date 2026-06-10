916b95060cf1e03565e5643c62dc83250e943738
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-10 20:25:06 +0300

    Implement Vue example

--- Vue/src/components/HomeContent.vue
+++ Vue/src/components/HomeContent.vue
@@ -1,28 +1,95 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import DxButton from 'devextreme-vue/button';
+import {
+  DxColumn,
+  DxDataGrid,
+  DxEditing,
+  DxLookup,
+  DxPaging,
+} from 'devextreme-vue/data-grid';
+import type {
+  CellClickEvent,
+  CellPreparedEvent,
+  EditorPreparingEvent,
+} from 'devextreme/ui/data_grid';
+import { employees, states } from '../data';
 
-const props = defineProps({
-  text: {
-    type: String,
-    default: 'count',
-  },
-});
-const count = ref(0);
-const buttonText = computed < string > (
-  () => `Click ${props.text}: ${count.value}`
-);
-function clickHandler() {
-  count.value += 1;
+let editCells: string[] = [];
+
+function onCellPrepared(e: CellPreparedEvent) {
+  if (e.rowType === 'data' && editCells.includes(`${e.rowIndex}:${e.columnIndex}`)) {
+    e.cellElement.style.backgroundColor = 'lightblue';
+  }
+}
+
+function onEditorPreparing(e: EditorPreparingEvent) {
+  const grid = e.component;
+  if (e.parentType === 'dataRow') {
+    const oldOnValueChanged = e.editorOptions.onValueChanged;
+    e.editorOptions.onValueChanged = function onValueChanged(
+      this: unknown,
+      args: { value: unknown },
+    ) {
+      oldOnValueChanged.apply(this, [args]);
+      editCells.forEach((cell) => {
+        const [rowIndex, columnIndex] = cell.split(':').map(Number);
+        grid.cellValue(rowIndex, columnIndex, args.value);
+      });
+    };
+  }
+}
+
+function onCellClick(e: CellClickEvent) {
+  if (e.event?.ctrlKey) {
+    editCells.push(`${e.rowIndex}:${e.columnIndex}`);
+  } else if (editCells.length) {
+    editCells = [];
+    e.component.repaint();
+  }
+}
+
+function resetEditCells() {
+  editCells = [];
 }
 </script>
 <template>
   <div>
-    <DxButton
-      :text="buttonText"
-      @click="clickHandler"
-    />
+    <DxDataGrid
+      :data-source="employees"
+      key-expr="ID"
+      @cell-prepared="onCellPrepared"
+      @editor-preparing="onEditorPreparing"
+      @cell-click="onCellClick"
+      @saving="resetEditCells"
+      @edit-canceled="resetEditCells"
+    >
+      <DxPaging :enabled="false"/>
+      <DxEditing
+        mode="batch"
+        :allow-updating="true"
+      />
+      <DxColumn
+        data-field="Prefix"
+        caption="Title"
+        :width="70"
+      />
+      <DxColumn data-field="FirstName"/>
+      <DxColumn data-field="LastName"/>
+      <DxColumn
+        data-field="Position"
+        :width="170"
+      />
+      <DxColumn
+        data-field="StateID"
+        caption="State"
+        :width="125"
+      >
+        <DxLookup
+          :data-source="states"
+          display-expr="Name"
+          value-expr="ID"
+        />
+      </DxColumn>
+    </DxDataGrid>
   </div>
 </template>
