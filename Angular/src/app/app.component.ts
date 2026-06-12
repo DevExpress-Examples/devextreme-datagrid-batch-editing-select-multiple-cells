b4facc3ec44731e6420722bf7e75e75698781b2c
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-12 11:22:47 +0300

    Update Angular to v22

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { DxDataGridModule, DxDataGridTypes } from 'devextreme-angular/ui/data-grid';
 import { Employee, Service, State } from './app.service';
 
@@ -7,6 +7,7 @@ import { Employee, Service, State } from './app.service';
     imports: [DxDataGridModule],
     providers: [Service],
     templateUrl: './app.component.html',
+    changeDetection: ChangeDetectionStrategy.Eager,
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
